212fa7d144c72ff186e4b4427c78745762927bcf
marcos <dev_marcos2024@outlook.com>
2025-06-29 15:15:45 -0300

    Criando menu logaut

--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -1,24 +1,16 @@
 import logo from "../../../assets/imgs/logo.svg";
-import { HeaderStyle } from "./style";
+import { HeaderStyle, MenuLogin, Nav, Menu } from "./style";
 import { Link } from "react-router-dom";
 import { useUser } from "../../hooks/useUser";
 import { FiUser, FiLogIn, FiLogOut } from "react-icons/fi";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const Header = () => {
-  const { isLogged, user, handleLogout } = useUser();
-  const navigate = useNavigate();
+  const { isLogged, user } = useUser();
+  const [open, setOpen] = useState(false);
 
-  function handleClick(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    const target = e.target as HTMLElement;
-    if (target.id) {
-      if (target.id === "none") {
-        navigate("/dashboard", { replace: false });
-      } else {
-        navigate("/", { replace: false });
-        handleLogout();
-      }
-    }
+  function openMenu() {
+    setOpen(!open);
   }
 
   return (
@@ -26,29 +18,40 @@ export const Header = () => {
       <Link to="/">
         <img src={logo} alt="Logotipo WebCarros" />
       </Link>
-      <div className="loginLoguot">
-        {isLogged && (
+
+      <MenuLogin>
+        {
           <p>
-            Bem vindo! <strong>{user.nome}</strong>...
+            Bem vindo! <strong>{user.nome}</strong>
           </p>
-        )}
-        <div className="login" onClick={handleClick}>
-          <Link id="login" to={`${isLogged ? "/dashboard" : "/login"}`}>
-            {!isLogged ? (
-              <FiLogIn size={20} color="#7d7d7d" />
-            ) : (
-              <FiUser size={20} color="#7d7d7d" />
-            )}
-          </Link>
-          {isLogged && (
-            <button id="none" className="none">
-              <div id="menu" className="menu">
-                Sair <FiLogOut />
-              </div>
+        }
+        <Nav>
+          {!isLogged && (
+            <button className="btn" onClick={openMenu}>
+              <FiLogIn size={18} color="#7d7d7d" />
             </button>
           )}
-        </div>
-      </div>
+          {isLogged && (
+            <button className="btn" onClick={openMenu}>
+              <FiUser size={18} color="#7d7d7d" />
+            </button>
+          )}
+        </Nav>
+        <Menu className={` menu ${open && "open"}`}>
+          <ul>
+            <li>
+              <Link to="/login">
+                Sair <FiLogOut />
+              </Link>
+            </li>
+            <li>
+              <Link to="/login">
+                Sair <FiLogOut />
+              </Link>
+            </li>
+          </ul>
+        </Menu>
+      </MenuLogin>
     </HeaderStyle>
   );
 };
--- src/shared/components/Header/style.ts
+++ src/shared/components/Header/style.ts
@@ -8,54 +8,55 @@ export const HeaderStyle = styled.header`
   background-color: #ffffff;
   padding: 0 30px;
   border-bottom: 2px solid #7d7d7d;
-  .login {
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 40px;
-    width: 40px;
+`;
+
+export const MenuLogin = styled.div`
+  position: relative;
+  display: flex;
+  color: #7d7d7d;
+  align-items: center;
+  gap: 12px;
+  .open {
+    max-height: 500px; /* valor grande o suficiente para caber os itens */
+  }
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+
+  .btn {
     border: 2px solid #7d7d7d;
     border-radius: 50%;
+    padding: 8px;
+    cursor: pointer;
+  }
+`;
 
+export const Menu = styled.div`
+  position: absolute;
+  top: 110%; /* posiciona o menu logo abaixo do botão */
+  display: flex;
+  right: 0;
+  flex-direction: column;
+  max-height: 0;
+  overflow: hidden;
+  transition: all 0.3s ease;
+  background: #7d7d7d;
+  color: #ffffff;
+  border-radius: 8px;
+
+  li a {
+    display: flex;
+    align-items: center;
+    margin: 10px;
+    gap: 8px;
+    padding: 0 10px;
+    cursor: pointer;
+    border-radius: 4px;
+    transition: all 1s;
     :hover {
-      .none {
-        display: flex;
-        cursor: pointer;
-      }
+      background-color: rgb(100, 100, 100);
     }
-  }
-  .loginLoguot {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 10px;
-    color: #7d7d7d;
-  }
-  .menu {
-    background-color: #7d7d7d;
-    border-radius: 8px;
-    padding: 1rem 0.5rem;
-    color: #ffffff;
-    font-size: 14px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 5px;
-    width: 70px;
-  }
-  .none {
-    position: absolute;
-    display: none;
-    align-items: end;
-    height: 100px;
-    top: 0;
-  }
-
-  #login:hover {
-    background-color: red;
-  }
-  #login {
-    z-index: 2;
   }
 `;
